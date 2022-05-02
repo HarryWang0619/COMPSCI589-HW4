911be09e7defa1b4cbebbbbc894c240ab1397984
Harry <harrywang0619@gmail.com>
2022-05-02 12:51:36 -0400

    init weight finish

---
+++ neuralnetwork.py
@@ -0,0 +1,29 @@
+from utils import *
+from stratified import *
+
+def initialize_weights(ohe_category,layer_parameter, biasterm=True):
+    weight_matrix_list = []
+
+    inputcategory, outputcategory = [],[]
+    inputindex, outputindex = [],[]
+    n = 0
+    for i in ohe_category:
+        if ohe_category[i] != 'class_numerical':
+            inputcategory.append(i) # name of the input category
+            inputindex.append(n) # index of the input category
+        else:
+            outputcategory.append(i) # name of the output category  
+            outputindex.append(n) # index of the output category
+        n += 1
+    
+    b = 1 if biasterm == True else 0
+    
+    updatedlayerparameterwbias = [len(inputcategory)+b] + list(np.array(layer_parameter)+b) + [len(outputcategory)] # [inputlayer, layerparameters, outputlayer]
+    for i in range(len(updatedlayerparameterwbias)-1):
+        layernow = updatedlayerparameterwbias[i]
+        layernext = updatedlayerparameterwbias[i+1]-1 if i !=len(updatedlayerparameterwbias)-2 else updatedlayerparameterwbias[i+1] 
+        # ^ for the last layer, the bias is not included, so don't need to minus 1 ^
+        init_weight = np.random.rand(layernext,layernow) * 2 - 1 # initialize the weight with random number between -1 and 1
+        weight_matrix_list.append(init_weight)
+        
+    return weight_matrix_list
