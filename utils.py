cf4ab719316a61198dac5e28448555db7893f95d
Harry <harrywang0619@gmail.com>
2022-05-01 22:22:24 -0400

    onehotted

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ from sqlite3 import Row
 from evaluationmatrix import *
 from sklearn import datasets
 import sklearn.model_selection
+from sklearn.preprocessing import OneHotEncoder
 import random
 import numpy as np
 import csv
@@ -18,6 +19,40 @@ def importfile(name:str,delimit:str):
         dataset.append(row)
     file.close()
     return dataset
+
+def onehotencoder(data,category):
+    dataT=data.T.copy()
+    enc = OneHotEncoder(sparse=False)
+    i = 0
+    appendeddict = {}
+    for cat in category:
+        if category[cat] == 'categorical':
+            hotneeded = dataT[i]
+            hotted = enc.fit_transform(hotneeded.reshape(-1,1))
+            for j in enc.categories_[0]:
+                newname = cat+'_'+str(j)
+                appendeddict[newname] = 'numerical'
+            dataT = np.append(dataT,hotted.T,axis=0)
+        if category[cat] == 'class':
+            hotneeded = dataT[i]
+            hotted = enc.fit_transform(hotneeded.reshape(-1,1))
+            for class_ in enc.categories_[0]:
+                newname = cat+'_'+str(class_)
+                appendeddict[newname] = 'class_numerical'
+            dataT = np.append(dataT,hotted.T,axis=0)
+        i += 1
+    
+    category.update(appendeddict)
+    i = 0
+    categorycopy = category.copy()
+    droplist = []
+    for cat in category:
+        if category[cat] == 'categorical' or category[cat] == 'class':
+            droplist.append(i)
+            categorycopy.pop(cat)
+        i += 1
+    dataT = np.delete(dataT,droplist,axis=0)
+    return dataT.T, categorycopy
 
 def same(attributecolumn):
     return all(item == attributecolumn[0] for item in attributecolumn)
