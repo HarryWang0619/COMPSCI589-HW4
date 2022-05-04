0dfccbfc68c8f693d253ef67e862db283c9d621a
Harry <harrywang0619@gmail.com>
2022-05-03 20:41:00 -0400

    finish all example

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -35,9 +35,9 @@ def costfunction(expected_output, actual_output):
 def sumofweights(listofweights,bias=True): # computes the square of all weights of the network and sum them up
     sum = 0
     for weight in listofweights:
-        w = weight.copy()
-        w[:, 0] = 0
         if bias:
+            w = weight.copy()
+            w[:, 0] = 0
             sum += np.sum(np.square(w))
         else:
             sum += np.sum(np.square(weight))
